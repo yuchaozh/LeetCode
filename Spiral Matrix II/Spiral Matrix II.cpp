7740d86437cd429977ad90e975bb18d4116e01f6
Zhou Yuchao <513629287@qq.com>
2014-07-05 13:48:32 -0500

    add spiral matrix II

--- Spiral Matrix II/Spiral Matrix II.cpp
+++ Spiral Matrix II/Spiral Matrix II.cpp
@@ -28,7 +28,34 @@ public:
         vector<vector<int> > res(n, vector<int>(n, 0));
         if (n == 0) {return res;}
         int i = 1;
-        int x = 0;
-        int y = 0;
+        int x = 0; // row
+        int y = 0; // col
+        res[0][0] = i++;
+        while (i <= n * n)
+        {
+            // if res[?][?] != 0 means it is the last one, needs trun right
+            // y + 1 < n: not exceed the right edge
+            while (y + 1 < n && res[x][y + 1] == 0) // keep going right
+            {
+                res[x][++y] = i++;
+            }
+            // x + 1 < n: not exceed the button edge
+            while (x + 1 < n && res[x + 1][y] == 0) // keep going down
+            {
+                res[++x][y] = i++;
+            }
+            // y - 1 >= 0: not exceed the left edge
+            while (y - 1 >= 0 && res[x][y - 1] == 0) // keep going left
+            {
+                res[x][--y] = i++;
+            }
+            // x - 1 >= 0: not exceed the top edge
+            // use res[x-1][y] to check the ahead element
+            while (x - 1 >= 0 && res[x - 1][y] == 0)
+            {
+                res[--x][y] = i++;
+            }
+        }
+        return res;
     }
 };
